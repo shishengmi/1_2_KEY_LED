d4b3f7aba5da63cce2682f3fa560446acf54e679
shishengmi <shishengmi@outlook.com>
2023-09-17 13:26:51 +0800

    初始化

---
+++ cmake-build-debug/CMakeFiles/1_2_KEY_LED.elf.dir/DependInfo.cmake
@@ -0,0 +1,64 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/STM32/HAL/1_2_KEY_LED/Core/Startup/startup_stm32f407zgtx.s" "D:/STM32/HAL/1_2_KEY_LED/cmake-build-debug/CMakeFiles/1_2_KEY_LED.elf.dir/Core/Startup/startup_stm32f407zgtx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "D:/STM32/HAL/1_2_KEY_LED/Core/Inc"
+  "D:/STM32/HAL/1_2_KEY_LED/Drivers/STM32F4xx_HAL_Driver/Inc"
+  "D:/STM32/HAL/1_2_KEY_LED/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "D:/STM32/HAL/1_2_KEY_LED/Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "D:/STM32/HAL/1_2_KEY_LED/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/STM32/HAL/1_2_KEY_LED/Core/Src/gpio.c" "CMakeFiles/1_2_KEY_LED.elf.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/1_2_KEY_LED.elf.dir/Core/Src/gpio.c.obj.d"
+  "D:/STM32/HAL/1_2_KEY_LED/Core/Src/main.c" "CMakeFiles/1_2_KEY_LED.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/1_2_KEY_LED.elf.dir/Core/Src/main.c.obj.d"
+  "D:/STM32/HAL/1_2_KEY_LED/Core/Src/stm32f4xx_hal_msp.c" "CMakeFiles/1_2_KEY_LED.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj" "gcc" "CMakeFiles/1_2_KEY_LED.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj.d"
+  "D:/STM32/HAL/1_2_KEY_LED/Core/Src/stm32f4xx_it.c" "CMakeFiles/1_2_KEY_LED.elf.dir/Core/Src/stm32f4xx_it.c.obj" "gcc" "CMakeFiles/1_2_KEY_LED.elf.dir/Core/Src/stm32f4xx_it.c.obj.d"
+  "D:/STM32/HAL/1_2_KEY_LED/Core/Src/syscalls.c" "CMakeFiles/1_2_KEY_LED.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/1_2_KEY_LED.elf.dir/Core/Src/syscalls.c.obj.d"
+  "D:/STM32/HAL/1_2_KEY_LED/Core/Src/sysmem.c" "CMakeFiles/1_2_KEY_LED.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/1_2_KEY_LED.elf.dir/Core/Src/sysmem.c.obj.d"
+  "D:/STM32/HAL/1_2_KEY_LED/Core/Src/system_stm32f4xx.c" "CMakeFiles/1_2_KEY_LED.elf.dir/Core/Src/system_stm32f4xx.c.obj" "gcc" "CMakeFiles/1_2_KEY_LED.elf.dir/Core/Src/system_stm32f4xx.c.obj.d"
+  "D:/STM32/HAL/1_2_KEY_LED/Core/Src/usart.c" "CMakeFiles/1_2_KEY_LED.elf.dir/Core/Src/usart.c.obj" "gcc" "CMakeFiles/1_2_KEY_LED.elf.dir/Core/Src/usart.c.obj.d"
+  "D:/STM32/HAL/1_2_KEY_LED/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj" "gcc" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj.d"
+  "D:/STM32/HAL/1_2_KEY_LED/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj.d"
+  "D:/STM32/HAL/1_2_KEY_LED/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj" "gcc" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj.d"
+  "D:/STM32/HAL/1_2_KEY_LED/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj.d"
+  "D:/STM32/HAL/1_2_KEY_LED/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj" "gcc" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj.d"
+  "D:/STM32/HAL/1_2_KEY_LED/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj" "gcc" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj.d"
+  "D:/STM32/HAL/1_2_KEY_LED/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj.d"
+  "D:/STM32/HAL/1_2_KEY_LED/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj.d"
+  "D:/STM32/HAL/1_2_KEY_LED/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj.d"
+  "D:/STM32/HAL/1_2_KEY_LED/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj.d"
+  "D:/STM32/HAL/1_2_KEY_LED/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj.d"
+  "D:/STM32/HAL/1_2_KEY_LED/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj.d"
+  "D:/STM32/HAL/1_2_KEY_LED/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj.d"
+  "D:/STM32/HAL/1_2_KEY_LED/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj" "gcc" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj.d"
+  "D:/STM32/HAL/1_2_KEY_LED/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj.d"
+  "D:/STM32/HAL/1_2_KEY_LED/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj" "gcc" "CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj.d
@@ -0,0 +1,36 @@
+CMakeFiles/1_2_KEY_LED.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj: \
+ D:\STM32\HAL\1_2_KEY_LED\Drivers\STM32F4xx_HAL_Driver\Src\stm32f4xx_hal_flash.c \
+ D:\STM32\HAL\1_2_KEY_LED\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal.h \
+ D:\STM32\HAL\1_2_KEY_LED\Core\Inc/stm32f4xx_hal_conf.h \
+ D:\STM32\HAL\1_2_KEY_LED\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_rcc.h \
+ D:\STM32\HAL\1_2_KEY_LED\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_def.h \
+ D:\STM32\HAL\1_2_KEY_LED\Drivers\CMSIS\Device\ST\STM32F4xx\Include/stm32f4xx.h \
+ D:\STM32\HAL\1_2_KEY_LED\Drivers\CMSIS\Device\ST\STM32F4xx\Include/stm32f407xx.h \
+ D:\STM32\HAL\1_2_KEY_LED\Drivers\CMSIS\Include/core_cm4.h \
+ c:\progra~2\gnuarm~1\102021~1.10\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ c:\progra~2\gnuarm~1\102021~1.10\arm-none-eabi\include\stdint.h \
+ c:\progra~2\gnuarm~1\102021~1.10\arm-none-eabi\include\machine\_default_types.h \
+ c:\progra~2\gnuarm~1\102021~1.10\arm-none-eabi\include\sys\features.h \
+ c:\progra~2\gnuarm~1\102021~1.10\arm-none-eabi\include\_newlib_version.h \
+ c:\progra~2\gnuarm~1\102021~1.10\arm-none-eabi\include\sys\_intsup.h \
+ c:\progra~2\gnuarm~1\102021~1.10\arm-none-eabi\include\sys\_stdint.h \
+ D:\STM32\HAL\1_2_KEY_LED\Drivers\CMSIS\Include/cmsis_version.h \
+ D:\STM32\HAL\1_2_KEY_LED\Drivers\CMSIS\Include/cmsis_compiler.h \
+ D:\STM32\HAL\1_2_KEY_LED\Drivers\CMSIS\Include/cmsis_gcc.h \
+ D:\STM32\HAL\1_2_KEY_LED\Drivers\CMSIS\Include/mpu_armv7.h \
+ D:\STM32\HAL\1_2_KEY_LED\Drivers\CMSIS\Device\ST\STM32F4xx\Include/system_stm32f4xx.h \
+ D:\STM32\HAL\1_2_KEY_LED\Drivers\STM32F4xx_HAL_Driver\Inc/Legacy/stm32_hal_legacy.h \
+ c:\progra~2\gnuarm~1\102021~1.10\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ D:\STM32\HAL\1_2_KEY_LED\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_rcc_ex.h \
+ D:\STM32\HAL\1_2_KEY_LED\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_gpio.h \
+ D:\STM32\HAL\1_2_KEY_LED\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_gpio_ex.h \
+ D:\STM32\HAL\1_2_KEY_LED\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_exti.h \
+ D:\STM32\HAL\1_2_KEY_LED\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_dma.h \
+ D:\STM32\HAL\1_2_KEY_LED\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_dma_ex.h \
+ D:\STM32\HAL\1_2_KEY_LED\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_cortex.h \
+ D:\STM32\HAL\1_2_KEY_LED\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash.h \
+ D:\STM32\HAL\1_2_KEY_LED\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash_ex.h \
+ D:\STM32\HAL\1_2_KEY_LED\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash_ramfunc.h \
+ D:\STM32\HAL\1_2_KEY_LED\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_pwr.h \
+ D:\STM32\HAL\1_2_KEY_LED\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_pwr_ex.h \
+ D:\STM32\HAL\1_2_KEY_LED\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_uart.h
